640e68d9981d929e5693d5a643a953bd8dd3f3c6
cmj <chmin82@gmail.com>
2022-01-23 23:45:37 +0900

    .gitignore

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`server is running ${PORT}`)
+})
---
+++ readme.md
@@ -0,0 +1,18 @@
+## 따라하기 유튜브
+
+https://www.youtube.com/watch?v=UoKoPP91Qx0
+
+### 설치
+
+```
+$ mkdir express-socketio-chatroom-test
+
+$ cd express-socketio-chatroom-test
+
+# npm 초기화
+$ npm init
+
+# 설치
+$ npm install express socket.io moment
+
+$ 
