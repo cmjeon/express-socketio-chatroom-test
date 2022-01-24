7b7fd0372d4c08c2112fa6a320ddcfbb634528f5
cmj <chmin82@gmail.com>
2022-01-24 23:31:27 +0900

    프론트에서 입력한 내용을 서버에서 받기

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,6 +1,19 @@
 'use strict'
 const socket = io();
 
+const nickname = document.querySelector('#nickname');
+const chatlist = document.querySelector('.chatting-list');
+const chatInput = document.querySelector('.chatting-input');
+const sendButton = document.querySelector('.send-button')
+
+sendButton.addEventListener("click", () => {
+  const param = {
+    name:nickname.value,
+    msg:chatInput.value,
+  }
+  socket.emit("chatting1", param)
+})
+
 socket.emit('chatting1', "from front")
 
 socket.on('chatting1', (data) => {
